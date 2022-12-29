5741e3628abbbf8ce7cd02cd07f508467cffcad0
sianturiwilly <prajudiwilliam10@gmail.com>
2022-12-29 14:47:35 +0700

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <h2>{{ text }}</h2>
+    <button @click="changeText">Ubah Text</button>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    props: ["text"],
+    data(){
+        return{
+            newText: "Ini adalah teks dari Header.vue.",
+        }
+    },
+    methods: {
+        changeText(){
+            this.$emit("changeTitle", this.newText);
+        },
+    },
+    emits: ["changeTitle"],
+};
+</script>
+
+<style>
+</style>
